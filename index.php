bb6449062219e78a0aa31c7d47b3aa101c811d32
andriusmw <andriusmw@yahoo.es>
2024-12-05 11:59:18 +0100

    else ifs

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 
 <?php 
+    //VARIABLES
     $name = "Usuario";
     $isDev = true;
     $age = 39;
 
+    //Booleans + Ifs
+    $isOld = $age > 40;
+
+    if($isOld) {
+        echo "<h2>Eres mayor, lo siento</h2>";
+    } else {
+        echo "<h2>Disfruta mientras puedas xD</h2>";
+    }
+
+    //CONSTANTE GLOBAL
     //Crea una constante, primero indica el nombre de la constante y luego el contenido
     define('PHP_LOGO', 'https://cdn.freebiesupply.com/logos/large/2x/php-logo-svg-vector.svg');
     
@@ -22,7 +33,11 @@
     echo gettype($age);
 
        
-     $output= "Este es un tecto de prueba para $name"  
+     $output= "Este es un texto de prueba para $name"  ;
+
+     //CONSTANTES LOCALES
+
+    const NOMBRE = "Paquito";
 
 ?>
 
@@ -32,8 +47,32 @@
 <h1><?= "Bienvenido " . $name ?></h1>
 
 <h2><?= $output ?></h2>
+<h2><?= NOMBRE ?></h2>
 
 <img src="<?= PHP_LOGO ?>" alt="PHP LOGO" width="200">
+
+
+
+<?php if ($isOld) : ?>
+    <h2>Eres mayor, lo siento</h2>
+
+    <?php elseif ($isDev) : ?>
+        <h2>No eres mayor, pero eres Dev</h2>
+
+<?php else : ?>
+    <h2>Disfrutas mientras eres jóven</h2>
+
+<?php endif; ?>
+
+
+
+
+
+
+
+
+
+
 
 
 <style>
